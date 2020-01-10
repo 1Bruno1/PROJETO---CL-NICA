0d66329de2400d2a89528bda24f2b24b9a7b5d77
1Bruno1 <apenasumbruno@gmail.com>
2020-01-10 17:46:35 -0300

    first commit

---
+++ footer.php
@@ -0,0 +1,6 @@
+<footer>
+    Copyright &copy; - Instagram: @__bru.no
+</footer>
+
+</body>    <!-- fecha o corpo do site (body) e o rodapé (footer) para serem unidos dps no index (como se fosse a classe main) -->
+</html>
---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>      <!--   código gerado digitando = "html: 5"  -->
+<html lang="pt-BR">
+
+<head>  <!-- comentário -->
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/estilo.css">   <!--   stylesheet são estilos diferentes do CSS  -->
+                                                    <!--   precisa identificar a paste e o arquivo   -->
+                                                    <!--   exemplo: "nome da pasta/ nome do arquivo.css   -->
+                                                    <!--   exemplo: "css/estilo.css   -->
+
+    <title>Document</title>
+
+</head>
+
+<body>  
+          <!-- abre o corpo do site (body) e deixa em aberto para adicionar coisas -->
+    <header>    <!-- o cabeçalho (header) precisa estar dentro do corpo (body) -->
+
+    <div class="container cabeçalho">
+    <img id="img-logo" src="img/umbrella.png" alt="logo">
+    <span id="nome-logo">Clínica Umbrella</span>
+    </div>
+
+    </header>
